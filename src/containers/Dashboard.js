01b945ee1a5c71c3e9c4943c97fc2947d9e43d8e
amberstyers <amberstyers@gmail.com>
2020-02-05 20:40:30 -0600

    added user

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux'
+import Dashboard from '../components/Dashboard'
+
+
+const mapStateToProps = (state) => {
+  return {
+      cars: state.cars
+      
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
